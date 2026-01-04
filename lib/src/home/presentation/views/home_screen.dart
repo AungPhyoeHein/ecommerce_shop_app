02b8b696c9245cdd88d33968acdf88f2f8f8be78
JERRY <jerry@jerry.lan>
2026-01-04 19:51:50 +0630

    feat: implement home drawer UI structure

--- lib/src/home/presentation/views/home_screen.dart
+++ lib/src/home/presentation/views/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_shop_app/core/common/app/providers/user_provider.dart';
+import 'package:ecommerce_shop_app/core/entities/user.dart';
 import 'package:ecommerce_shop_app/core/extensions/text_style_extension.dart';
 import 'package:ecommerce_shop_app/core/res/styles/colors.dart';
 import 'package:ecommerce_shop_app/core/res/styles/text.dart';
@@ -6,6 +8,7 @@ import 'package:ecommerce_shop_app/core/utils/core_utils.dart';
 import 'package:ecommerce_shop_app/core/widgets/app_bar_bottom.dart';
 import 'package:ecommerce_shop_app/core/widgets/ecomi_logo.dart';
 import 'package:ecommerce_shop_app/core/widgets/products_list_widget.dart';
+import 'package:ecommerce_shop_app/core/widgets/rounded_button.dart';
 import 'package:ecommerce_shop_app/core/widgets/svg_icon.dart';
 import 'package:ecommerce_shop_app/src/category/presentation/app/adapter/category_cubit.dart';
 import 'package:ecommerce_shop_app/src/home/presentation/widgets/category_list_widget.dart';
@@ -15,6 +18,7 @@ import 'package:ecommerce_shop_app/src/product/presentation/app/adapter/product_
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:hugeicons_pro/hugeicons.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -55,6 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final User user = UserProvider.instance.currentUser!;
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -119,6 +124,95 @@ class _HomeScreenState extends State<HomeScreen> {
           context,
           lightModeColor: MyColors.lightThemeStockColor,
           darkModeColor: MyColors.darkThemeDarkNavBarColor,
+        ),
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                radius: 20,
+                backgroundColor: MyColors.lightThemePrimaryTint,
+                child: Text(user.name[0], style: TextStyles.appLogo),
+              ),
+              accountName: Text(
+                user.name,
+                style: TextStyles.headingMedium3.adaptiveColor(context),
+              ),
+              accountEmail: Text(user.email),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Profile',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.profile02,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Edit',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.edit01,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Wishlist',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.favourite,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Orders History',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.transactionHistory,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Privacy Policy',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.securityCheck,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    title: Text(
+                      'Terms & Conditions',
+                      style: TextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    leading: Icon(
+                      HugeIconsStroke.documentValidation,
+                      color: MyColors.classicAdaptiveTextColor(context),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            RoundedButton(text: "Logout"),
+          ],
         ),
       ),
     );
